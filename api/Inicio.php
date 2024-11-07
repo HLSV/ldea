03050bf30e262f35d061b8ffabe3966628b2a2ec
Igor <hackerlgsecvir@gmail.com>
2024-11-07 18:06:30 -0300

    Add files via upload
    Signed-off-by: Igor <hackerlgsecvir@gmail.com>

--- api/Inicio.php
+++ api/Inicio.php
@@ -74,10 +74,10 @@ $visitCount = $_SESSION['visit_count'];
 Novidade (05/04/2024)  plataforma de Boletim de segurança digital  <a href="../Boletim de segurança digital/Boletim.html">click aqui</a>
             </p>
            <p>
-Novidade (07/11/2024)  Comande o site com sua voz e explore temas e funcionalidades!  <a href="Pasta1/Site por voz.html">click aqui</a>
+Novidade (07/11/2024)  Comande o site com sua voz e explore temas e funcionalidades!  <a href="../Site por voz.html">click aqui</a>
             </p>
   <p>
-Novidade (07/11/2024) pix postPix_postagem_fã   <a href="/Pasta1/Pix_postagem_fa.html">click aqui</a>
+Novidade (07/11/2024) pix postPix_postagem_fã   <a href="../Pix_postagem_fa.html">click aqui</a>
             </p>
         </div>
     </div>
